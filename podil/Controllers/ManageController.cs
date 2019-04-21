25d896109e179a05c3aab2ab83c3726af089011a
Ravinder Bhatia <ravinderb221@gmail.com>
2019-04-21 13:14:50 -0700

    moving classes from Manage and Account models to ViewModels/Account and Manage locations

--- podil/Controllers/ManageController.cs
+++ podil/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using podil.Models;
+using podil.ViewModels.Manage;
 
 namespace podil.Controllers
 {
---
+++ podil/ViewModels/Account/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace podil.ViewModels.Account
+{
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
