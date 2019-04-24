501bbb40fbabaad910b9729a578b3562fbbe4b48
Ravinder Bhatia <ravinderb221@gmail.com>
2019-04-23 20:05:41 -0700

    adding public folder to gitignore

---
+++ podil/Migrations/201904240220517_AddDescriptionAndFileNameToPhoto.cs
@@ -0,0 +1,22 @@
+namespace podil.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDescriptionAndFileNameToPhoto : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Photos", "Description", c => c.String());
+            AddColumn("dbo.Photos", "FileName", c => c.String());
+            DropColumn("dbo.Photos", "PhotoUrl");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Photos", "PhotoUrl", c => c.String());
+            DropColumn("dbo.Photos", "FileName");
+            DropColumn("dbo.Photos", "Description");
+        }
+    }
+}
